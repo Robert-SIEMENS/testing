b7f7da7b6e1b1ed3a3a1b3e6693ceed00f071c8a
Robert-SIEMENS <115344163+Robert-SIEMENS@users.noreply.github.com>
2023-03-05 23:09:34 +0400

    Update server.c

--- server.c
+++ server.c
@@ -5,74 +5,87 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-#define PORT 8080
-#define MAX_COMMAND_SIZE 1024
+void handle_command(int client_socket, char* command);
 
-int main(int argc, char const *argv[]) {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_address, client_address;
+    char buffer[1024];
     int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[MAX_COMMAND_SIZE] = {0};
-    FILE *fp;
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    socklen_t client_address_length = sizeof(client_address);
+    
+    // Create the server socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        printf("Failed to create server socket\n");
+        return -1;
     }
-
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+    
+    // Set the socket options
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        printf("Failed to set socket options\n");
+        return -1;
     }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    
+    // Bind the socket to a port
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(8080);
+    if (bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address)) == -1) {
+        printf("Failed to bind server socket\n");
+        return -1;
     }
-
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
+    
+    // Listen for incoming connections
+    if (listen(server_socket, 5) == -1) {
+        printf("Failed to listen for incoming connections\n");
+        return -1;
     }
-
+    
+    // Accept incoming connections
     while (1) {
-        printf("\nListening for incoming connection...\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
+        client_socket = accept(server_socket, (struct sockaddr*) &client_address, &client_address_length);
+        if (client_socket == -1) {
+            printf("Failed to accept incoming connection\n");
+            continue;
         }
-
-        printf("Connection accepted.\n");
-        while (1) {
-            int valread;
-            valread = read(new_socket, buffer, MAX_COMMAND_SIZE);
-            if (valread <= 0) break;
-
-            printf("Received command: %s", buffer);
-            fp = popen(buffer, "r");
-            if (fp == NULL) {
-                printf("Failed to run command\n");
-                exit(EXIT_FAILURE);
-            }
-
-            char response[MAX_COMMAND_SIZE] = {0};
-            while (fgets(response, MAX_COMMAND_SIZE, fp)) {
-                send(new_socket, response, strlen(response), 0);
-                printf("%s", response);
-                memset(response, 0, MAX_COMMAND_SIZE);
-            }
-
-            pclose(fp);
-            send(new_socket, "\n", 1, 0);
-            printf("\nCommand executed.\n");
+        
+        // Receive the command from the client
+        memset(buffer, 0, sizeof(buffer));
+        if (recv(client_socket, buffer, sizeof(buffer), 0) == -1) {
+            printf("Failed to receive command from client\n");
+            continue;
         }
-        close(new_socket);
-        printf("Connection closed.\n");
+        
+        // Handle the command
+        handle_command(client_socket, buffer);
+        
+        // Close the client socket
+        close(client_socket);
     }
+    
+    // Close the server socket
+    close(server_socket);
+    
     return 0;
+}
+
+void handle_command(int client_socket, char* command) {
+    char output[1024];
+    
+    // Execute the command and capture the output
+    FILE *fp = popen(command + 6, "r");
+    if (fp == NULL) {
+        strcpy(output, "Command execution failed");
+    } else {
+        char buf[1024];
+        size_t n;
+        while ((n = fread(buf, 1, sizeof(buf), fp)) > 0) {
+            fwrite(buf, 1, n, stdout);
+        }
+        pclose(fp);
+    }
+    
+    // Send the output to the client
+    send(client_socket, output, strlen(output), 0);
 }
