11127cf42fe8126489622d4f3c4310301e086d74
Robert-SIEMENS <115344163+Robert-SIEMENS@users.noreply.github.com>
2023-03-05 22:18:15 +0400

    Update client.c

--- client.c
+++ client.c
@@ -1,78 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
+#include <arpa/inet.h>
 
-#define PORT 8080
 #define MAX_COMMAND_SIZE 1024
 
-int main(int argc, char const *argv[]) {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[MAX_COMMAND_SIZE] = {0};
-    FILE *fp;
+int main(int argc, char const *argv[])
+{
+  int sock =0;
+  struct sockaddr_in serv_addr;
+  char command[MAX_COMMAND_SIZE] = {0};
+  char response[MAX_COMMAND_SIZE] = {0};
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+  if(argc < 3)
+  { 
+   printf("Usage:%s <ip_address> <port>\n", argv[0]);
+   exit(EXIT_FAILURE);
+  }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+  if((sock = socket(AF_INET, SOCK_STREAM, 0))<0)
+  {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+  }
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(atoi(argv[2]));
+  
+  //CONVERT IP address from string to binary
+  if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
+  {
+    printf("invalid IP address\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
+  {
+    perror("connect failed");
+    exit(EXIT_FAILURE);
+  }
+  printf("Connected to server: %s:%s\n", argv[1], argv[2]);
+  while (1)
+  { 
+  printf("> ");
+  fgets(command, MAX_COMMAND_SIZE, stdin);
+  command[strcspn(command, "\n")]=0;
+  if(strcmp(command, "disconnect") == 0)
+  {
+    send(sock, command, strlen(command), 0);
+    printf("Disconnected from server.\n");
+    break;
+  }
+  else if(strncmp(command, "shell ", 6) == 0)
+  {
+   send(sock, command, strlen(command), 0);
+   printf("%s\n", response);
+  }
+  else
+  {
+  printf("Invalid command");
+  
+  }
+  memset(command, 0, sizeof(command));
+  memset(response, 0, sizeof(response));
+  
+  
+  }
+  close(sock);
+  return 0;
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
 
-    while (1) {
-        printf("\nListening for incoming connection...\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Connection accepted.\n");
-        while (1) {
-            int valread;
-            valread = read(new_socket, buffer, MAX_COMMAND_SIZE);
-            if (valread <= 0) break;
-
-            printf("Received command: %s", buffer);
-            fp = popen(buffer, "r");
-            if (fp == NULL) {
-                printf("Failed to run command\n");
-                exit(EXIT_FAILURE);
-            }
-
-            char response[MAX_COMMAND_SIZE] = {0};
-            while (fgets(response, MAX_COMMAND_SIZE, fp)) {
-                send(new_socket, response, strlen(response), 0);
-                printf("%s", response);
-                memset(response, 0, MAX_COMMAND_SIZE);
-            }
-
-            pclose(fp);
-            send(new_socket, "\n", 1, 0);
-            printf("\nCommand executed.\n");
-        }
-        close(new_socket);
-        printf("Connection closed.\n");
-    }
-    return 0;
 }
