1627592fbca60f3b798cda322f79f55f241fd39c
Robert-SIEMENS <115344163+Robert-SIEMENS@users.noreply.github.com>
2023-03-06 00:08:41 +0400

    Update server.c

--- server.c
+++ server.c
@@ -7,7 +7,8 @@
 
 void handle_command(int client_socket, char* command);
 
-int main() {
+int main()
+{
     int server_socket, client_socket;
     struct sockaddr_in server_address, client_address;
     char buffer[1024];
@@ -16,13 +17,15 @@ int main() {
     
     // Create the server socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
+    if (server_socket == -1) 
+    {
         printf("Failed to create server socket\n");
         return -1;
     }
     
     // Set the socket options
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
+    {
         printf("Failed to set socket options\n");
         return -1;
     }
@@ -31,28 +34,33 @@ int main() {
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(8080);
-    if (bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address)) == -1) {
+    if (bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address)) == -1)
+    {
         printf("Failed to bind server socket\n");
         return -1;
     }
     
     // Listen for incoming connections
-    if (listen(server_socket, 5) == -1) {
+    if (listen(server_socket, 5) == -1) 
+    {
         printf("Failed to listen for incoming connections\n");
         return -1;
     }
     
     // Accept incoming connections
-    while (1) {
+    while (1) 
+    {
         client_socket = accept(server_socket, (struct sockaddr*) &client_address, &client_address_length);
-        if (client_socket == -1) {
+        if (client_socket == -1)
+        {
             printf("Failed to accept incoming connection\n");
             continue;
         }
         
         // Receive the command from the client
         memset(buffer, 0, sizeof(buffer));
-        if (recv(client_socket, buffer, sizeof(buffer), 0) == -1) {
+        if (recv(client_socket, buffer, sizeof(buffer), 0) == -1) 
+        {
             printf("Failed to receive command from client\n");
             continue;
         }
@@ -70,17 +78,22 @@ int main() {
     return 0;
 }
 
-void handle_command(int client_socket, char* command) {
+void handle_command(int client_socket, char* command)
+{
     char output[1024];
     
     // Execute the command and capture the output
     FILE *fp = popen(command + 6, "r");
-    if (fp == NULL) {
+    if (fp == NULL) 
+    {
         strcpy(output, "Command execution failed");
-    } else {
+    }
+    else 
+    {
         char buf[1024];
         size_t n;
-        while ((n = fread(buf, 1, sizeof(buf), fp)) > 0) {
+        while ((n = fread(buf, 1, sizeof(buf), fp)) > 0) 
+        {
             fwrite(buf, 1, n, stdout);
         }
         pclose(fp);
